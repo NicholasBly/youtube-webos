74d5aabb4639a74c5acbb426894df7a6ec705a59
Nicholas Bly <73457207+NicholasBly@users.noreply.github.com>
2025-06-10 18:49:24 -0400

    Update sponsorblock.js

--- src/sponsorblock.js
+++ src/sponsorblock.js
@@ -239,7 +239,9 @@ class SponsorBlockHandler {
       if (this.sliderInterval) clearInterval(this.sliderInterval);
 
       this.sliderInterval = setInterval(() => {
-        this.slider = document.querySelector('.ytlr-progress-bar');
+          this.slider = document.querySelector(
+          '.ytlr-progress-bar, .ytLrProgressBarSlider'
+        );
         if (this.slider) {
           console.info('slider found...', this.slider);
           clearInterval(this.sliderInterval);
